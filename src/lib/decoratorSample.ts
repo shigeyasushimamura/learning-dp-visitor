43a76dc37451bba0a4c3811088eff0c987401baa
shigeyasu <sgys.mtk@gmail.com>
2025-05-16 21:37:05 +0900

    f

--- src/lib/decoratorSample.ts
+++ src/lib/decoratorSample.ts
@@ -88,14 +88,46 @@ abstract class SpellDecorator implements Spell {
 
 class Empowered extends SpellDecorator {
   cast(target: Character): void {
+    console.log("呪文が強化された！");
+    // まず元の呪文を発動
     this.decorated.cast(target);
+    // 追加ダメージ
     target.takeDamage(10);
   }
 }
 
-class QuickCoolDown extends SpellDecorator {
+class ManaEfficient extends SpellDecorator {
+  cast(target: Character): void {
+    console.log("マナ消費が半分になった！");
+    // ここでは元のcastを呼ぶだけでOK
+    this.decorated.cast(target);
+  }
+}
+class QuickCooldown extends SpellDecorator {
   cast(target: Character): void {
     this.decorated.cast(target);
     console.log("クールダウンが短くなった！");
   }
 }
+
+class Enemy implements Character {
+  takeDamage(damage: number): void {}
+}
+
+const enemy = new Enemy();
+
+// キャラは name, takeDamage を持っているものとします
+let spell: Spell = new Fireball();
+
+// 呪文に強化とクールダウン短縮を重ねる
+spell = new Empowered(spell);
+spell = new QuickCooldown(spell);
+
+// 発動するとログも副作用も含めて順番に処理される
+spell.cast(enemy);
+/*
+  呪文が強化された！
+  Goblin に火の玉をぶつけた！
+  Goblin に10の追加ダメージ！
+  クールダウンが短くなった！
+*/
